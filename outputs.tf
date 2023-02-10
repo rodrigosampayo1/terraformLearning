aede50e860c788a74e85b67a2fab94925dc0379b
rodrigosampayo1 <rodrigosampayo1@gmail.com>
2023-02-10 14:52:06 -0300

    providers

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "instance_ip" {
     description = "VM's private ip"
     value       = aws_instance.app_server.private_ip
 }
+
+#El output aparece despues de ejecutar terraform apply
+#or execution
